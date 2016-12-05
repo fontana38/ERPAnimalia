c020749276d4ae7a90f6df538c0637e52c8923b4
fontanamaria37 <fontana.maria37@gmail.com>
2016-12-05 18:44:13 -0300

    Add project files.

---
+++ ERPAnimalia/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ERPAnimalia.MvcApplication" Language="C#" %>
---
+++ ERPAnimalia/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ERPAnimalia.Startup))]
+namespace ERPAnimalia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
